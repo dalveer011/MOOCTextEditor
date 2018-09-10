0cd401d1d4282a97ece223a247d6af9088c5ea32
Dalveer Singh <dalvirsahota@gmail.com>
2018-09-10 16:18:24 -0400

    adds linked list implementation

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+public class ListNode<T> {
+	
+	private T data;
+	private ListNode<T> prev;
+	private ListNode<T> next;
+	
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public ListNode<T> getPrev() {
+		return prev;
+	}
+	public void setPrev(ListNode<T> prev) {
+		this.prev = prev;
+	}
+	public ListNode<T> getNext() {
+		return next;
+	}
+	public void setNext(ListNode<T> next) {
+		this.next = next;
+	}
+		
+}
---
+++ src/linkedlist/MyLinkedList.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class MyLinkedList<T> {
+	private ListNode<T> head;
+	private ListNode<T> tail;
+	
+	public ListNode<T> getHead() {
+		return head;
+	}
+	public void setHead(ListNode<T> head) {
+		this.head = head;
+	}
+	public ListNode<T> getTail() {
+		return tail;
+	}
+	public void setTail(ListNode<T> tail) {
+		this.tail = tail;
+	}
+	
+	public  void add(ListNode<T> data) {
+		
+	}
+	public int size() {
+		return 0;
+	}
+	
+}
